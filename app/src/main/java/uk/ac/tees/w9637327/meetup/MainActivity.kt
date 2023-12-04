59e5b91c96a9537735b03df4d8d95556f327e2eb
nelsonchristian <njchristian310@gmail.com>
2023-12-04 18:53:00 +0000

    login screen changes

--- app/src/main/java/uk/ac/tees/w9637327/meetup/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/w9637327/meetup/MainActivity.kt
@@ -3,44 +3,14 @@ package uk.ac.tees.w9637327.meetup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import uk.ac.tees.w9637327.meetup.ui.theme.MeetUpTheme
+import uk.ac.tees.w9637327.meetup.screens.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MeetUpTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            LoginScreen()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MeetUpTheme {
-        Greeting("Android")
-    }
-}
